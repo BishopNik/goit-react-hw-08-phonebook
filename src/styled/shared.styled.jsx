fd1267df1387bbc26eaf38e50a8d5e24aedbf7c9
BishopNik <128685383+BishopNik@users.noreply.github.com>
2023-10-23 16:30:38 +0200

    Button

--- src/styled/shared.styled.jsx
+++ src/styled/shared.styled.jsx
@@ -24,12 +24,12 @@ export const Container = styled.div`
 
 export const Button = styled.button`
 	border-radius: ${p => p.theme.spacing(2)};
-	font-size: 22px;
+	font-size: 18px;
 	border: 1px solid rgba(0, 0, 0, 0.7);
 	cursor: pointer;
 	box-shadow: 0px 0px 4px 2px rgba(128, 128, 128, 0.5);
 	transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1);
-	min-width: 50px;
+	min-width: 30px;
 	height: 34px;
 
 	&:hover {
@@ -38,6 +38,7 @@ export const Button = styled.button`
 	}
 
 	@media screen and (min-width: 768px) {
+		font-size: 22px;
 		min-width: 80px;
 		height: 40px;
 	}
